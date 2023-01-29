848f1a39160fbd17502e714ea13e411b5d376813
Youssef Emad <96674363+Yossef-Emad@users.noreply.github.com>
2023-01-29 20:11:56 +0200

    Create compress.java

---
+++ compress.java
@@ -0,0 +1,164 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class compress {
+    String comp=new String();
+    String exp=new String("");
+    File file;
+    Scanner sc;
+    compress(String s) throws FileNotFoundException {
+        Minify v= new Minify(s);
+        this.exp=v.toMinify();
+    }
+
+
+
+    public static Node Tree(String s){
+        //check string not empty
+        if(s.length()==0||s==null){return null;}
+        //using hashmap for freq array
+        HashMap<Character,Integer> mp=new HashMap<>();
+        //
+        for(int i=0;i<s.length();i++){
+            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+        }
+        //
+        PriorityQueue<Node> pq=new PriorityQueue<>(Comparator.comparingInt(value -> value.data));
+        for(var en:mp.entrySet()){
+            pq.add(new Node(en.getKey(), en.getValue()));
+        }
+        while(pq.size()!=1){
+            Node left=pq.poll();
+            Node right=pq.poll();
+            int sum=left.data+right.data;
+            pq.add(new Node(null,sum,left,right));
+        }
+
+        return pq.peek();
+
+
+    }
+
+
+    public static String compressor(Node root, String str, Map<Character, String> ctr, String s){
+
+
+
+        comp(root, str, ctr);
+        StringBuilder sb = new StringBuilder("");
+        for (char c: s.toCharArray())
+        {
+            sb.append(ctr.get(c));
+        }
+
+
+
+        BigInteger b=new BigInteger(sb.toString(),2);
+        String y=new String(b.toByteArray());
+        String x=new String(sb.toString());
+        String d=new String("");
+        for (int i=0;i<y.length();i++) {
+            if(x.charAt(i)==' '){}
+            else{d+=x.charAt(i);}
+        }
+        String k=new String("");
+
+
+        int len=sb.length();
+        int p=0;
+        for (int i = 0; i < len; i += 7) {
+            int end = Math.min(i + 7, len);
+            String sevenBits = sb.substring(i, end);
+
+            char c=binaryToChar(sevenBits);
+            c+=20;
+           k+=c;
+
+        }
+
+        return k;
+    }
+    public static char binaryToChar(String binary) {
+        int decimal = Integer.parseInt(binary, 2);
+        return (char) decimal;
+    }
+
+    public static void comp(Node root, String str, Map<Character, String> ctr)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        if (isLeaf(root))
+        {
+            ctr.put(root.chData, str.length() > 0 ? str : "1");
+        }
+        comp(root.left_node, str + '0', ctr);
+        comp(root.right_node, str + '1', ctr);
+    }
+    public static String charToBinaryString(String str) {
+        StringBuilder binaryString = new StringBuilder();
+        int i=0;
+        for (char c : str.toCharArray()) {
+            i++;
+
+            c-=20;
+            int charCode = (int) c;
+            binaryString.append(String.format("%7s", Integer.toBinaryString(charCode)).replace(' ', '0'));
+            if(i==str.length()-2){break;}
+        }
+        return binaryString.toString();
+    }
+    public static String DeCompressor(Node root, int index, String s){
+        StringBuilder st=new StringBuilder("");
+         StringBuilder sb=new StringBuilder("");
+        String h=new String("");
+
+         h=charToBinaryString(s);
+        sb.append(h);
+
+
+        if (isLeaf(root))
+        {
+
+            while (root.data>0)
+            {
+                st.append(root.chData);
+                root.data--;
+            }
+        }
+        else
+        {
+
+
+            while (index < sb.length() - 1)
+            {
+                index= deComp(root, index, sb,st);
+            }
+        }
+        return st.toString();
+    }
+    public static Integer deComp(Node root, int index, StringBuilder sb,StringBuilder s)
+    {
+        if (isLeaf(root))
+        {  s.append(root.chData);
+            return index;
+        }
+        return deComp((sb.charAt(index) == '0') ? root.left_node : root.right_node, index+1, sb,s);
+
+    }
+
+
+    public static boolean isLeaf(Node root)
+    {
+        return root.left_node == null && root.right_node == null;
+    }
+}
