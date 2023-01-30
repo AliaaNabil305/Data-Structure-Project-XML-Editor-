48e34058afa9e90e40182293da336bb3d17d2293
Youssef Emad <96674363+Yossef-Emad@users.noreply.github.com>
2023-01-30 09:58:09 +0200

    Update compress.java

--- compress.java
+++ compress.java
@@ -162,4 +162,28 @@ public class compress {
     {
         return root.left_node == null && root.right_node == null;
     }
+    private static class Node {
+
+    public Node left;
+    public Node right;
+    Integer data;
+Character chData;
+ Node left_node=null;
+ Node right_node=null;
+ Node(char chData,int data){
+     this.chData=chData;
+     this.data=data;
+ }
+ Node(){}
+public Node(Character chData,Integer data,Node left,Node right){
+    this.chData=chData;
+    this.data=data;
+    this.left_node=left;
+    this.right_node=right;
+}
+
+
+
+
+}
 }
